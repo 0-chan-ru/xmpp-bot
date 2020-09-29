729d158a311c57b74f0aad0a00ce3bcc0070db8e
nekto-nekto <55918742+nekto-nekto@users.noreply.github.com>
2020-09-29 20:07:58 +0300

    plurar for misc.js getCountOnWebsocket message

--- scripts/misc.js
+++ scripts/misc.js
@@ -33,6 +33,22 @@ function getCountOnWebsocket(callback) {
 		console.error(error);
 	});
 }
+// plurar russian func via https://gist.github.com/tomfun/830fa6d8030d16007bbab50a5b21ef97
+function getNoun(number, one, two, five) {
+	let n = Math.abs(number);
+	n %= 100;
+	if (n >= 5 && n <= 20) {
+		return five;
+	}
+	n %= 10;
+	if (n === 1) {
+		return one;
+	}
+	if (n >= 2 && n <= 4) {
+		return two;
+	}
+	return five;
+}
 
 const groupchatFunc = function(conference, from, message, stamp, delay) {
 	if (delay && delay.stamp) { // костыль надо править библиотеку
@@ -42,7 +58,7 @@ const groupchatFunc = function(conference, from, message, stamp, delay) {
 		if (from !== options.main.nick) {
 			if (message === "!надоске") {
 				getCountOnWebsocket((anoncount) => {
-					xmpp.send(conference, `${from}: сидят на вебсокетах сейчас ${anoncount} анончиков`, true);
+					xmpp.send(conference, `${from}: сидят на вебсокетах сейчас ${anoncount} анонч${getNoun(anoncount, "ик", "ика", "иков")}`, true);
 				});
 			}
 		}
